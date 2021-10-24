b70dff61ee139e5cd3571745489583b7ab6ad146
gabriel <gabrielarturogq@ufps.edu.co>
2021-10-24 18:37:17 -0500

    update git

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ SaleRES(app);
 
 
 
-const PORT = 3100;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto http://localhost:${PORT}`);
